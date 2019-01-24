19b0fb8d025e469ef0e408d0c286b09c92f299f9
Alex Albright <alexanderjalbright@gmail.com>
2019-01-24 15:36:57 -0500

    added dictionaries

--- ArraysListsAndDictionaries/Program.cs
+++ ArraysListsAndDictionaries/Program.cs
@@ -7,10 +7,12 @@ namespace ArraysListsAndDictionaries
     {
         static void Main(string[] args)
         {
+            
             var fordFiesta = new Car("Ford", "Fiesta", 2013);
             var subaruLegacy = new Car("Subaru", "Legacy", 2017);
             var hondaCRV = new Car("Honda", "CRV", 2014);
 
+            //**********************************************************************************************
             // Array
             var parkedCars = new Car[5];
             Console.WriteLine(parkedCars.Length);
@@ -22,6 +24,7 @@ namespace ArraysListsAndDictionaries
 
 
 
+            //**********************************************************************************************
             // List
             // Examples
             var foo = new List<string>();
@@ -33,22 +36,95 @@ namespace ArraysListsAndDictionaries
             // List of Luxury Cars
             var luxuryCars = new List<Car>();
 
-            // Add to specific spot
-            luxuryCars[0] = hondaCRV;
+            
 
             // Add to next spot
             luxuryCars.Add(hondaCRV);
+
+            // Add to specific spot if spot already exists
+            luxuryCars[0] = hondaCRV;
+            luxuryCars.Insert(0,hondaCRV);
 
 
             var firstLuxuryCar = luxuryCars[0];
 
             Console.WriteLine(firstLuxuryCar);
             
-
+            for (int i = 0; i < luxuryCars.Count; i++)
+            {
+                var currentCar = luxuryCars[i];
+                Console.WriteLine(currentCar.Make);
+            }
             
+            // "foreach" does the same as for loop to a List
+            foreach(var currentCar in luxuryCars)
+            {
+                Console.WriteLine(currentCar.Make);
+            }
 
-            //
+            // Making object Examples
+            var carMakers = new List<string>();
+            carMakers.Add("Ford");
+            carMakers.Add("Chevy");
+            carMakers.Add("Kia");
+
+            var carMakers2 = new List<string>()
+            {
+                "Ford",
+                "Chevy",
+                "Kia"
+            };
+
+            var carMakers3 = new List<Car>()
+            {
+                new Car("Chevy", "Camaro", 1965),
+                new Car("Chevy", "Volt", 2015)
+                {
+                    MilesPerGallon = 12
+                }
+            };
+
+
+
+
+
+
+            //**********************************************************************************************
+            // Dictionaries
+
+            var students = new Dictionary<int, string>();
+            students.Add(1, "Nate");
+            students.Add(2, "Maya");
+
+            int scannedStudentId = 2;
+            var maya = students[scannedStudentId];
+
+            var configuration = new Dictionary<string, bool>()
+            {
+                {"AutoSave", true },
+                {"DarkMode", true }
+            };
+
+            var shouldIAutoSave = configuration["AutoSave"];
+
+            Console.WriteLine(shouldIAutoSave);
+
+            foreach(var setting in configuration)
+            {
+                Console.WriteLine(setting.Key + ": " + setting.Value);
+            }
             Console.ReadLine();
+
+            var phoneBook = new Dictionary<string, long>()
+            {
+                {"Alex Albright", 3307349959 },
+                {"Jenny", 3308675309 },
+                {"Joe Shmoe", 5555555 }
+            };
+
+            string choice = Console.ReadLine();
+
+            Console.WriteLine(phoneBook[choice]);
         }
     }
 }
